b61854f06eb64e57ad180fc91c035c19870de813
nicol <nicolassilva.ns16@gmail.com>
2024-06-01 20:53:22 -0300

    first commit

---
+++ src/MeuComponente.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MeuComponente = () => {
+  return (
+    <div>
+      <h2>Bem-vindo à minha página!</h2>
+      <p>Este é um parágrafo dentro do novo componente.</p>
+      <ul>
+      </ul>
+    </div>
+  );
+}
+
+export default MeuComponente;
